eaff0054a5032ae28c333c1ce875af7d22226509
James Cameron <james.cameron@octopus.com>
2022-12-13 09:23:25 +1000

    feat: day two part two

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -4,11 +4,56 @@ fn main() {
     let guide =
         fs::read_to_string("strategy_guide.txt").expect("Something went wrong reading the file");
 
-    let part_one_answer = play_game(&guide);
+    let part_one_answer = part_one(&guide);
     println!("Part one answer: {}", part_one_answer);
+
+    let part_two_answer = part_two(&guide);
+    println!("Part two answer: {}", part_two_answer);
 }
 
-pub fn play_game(strategy_guide: &str) -> i32 {
+fn get_tool_name(tool: char) -> &'static str {
+    match tool {
+        'X' | 'A' => "Rock",
+        'Y' | 'B' => "Paper",
+        'Z' | 'C' => "Scissors",
+        _ => "Unknown",
+    }
+}
+
+fn get_choice_from_outcome(opponent_choice: char, outcome: char) -> char {
+    match outcome {
+        'X' => {
+            // You need to lose
+            match opponent_choice {
+                'A' => 'Z',
+                'B' => 'X',
+                'C' => 'Y',
+                _ => 'X', // Will never happen
+            }
+        }
+        'Y' => {
+            // You need to draw
+            match opponent_choice {
+                'A' => 'X',
+                'B' => 'Y',
+                'C' => 'Z',
+                _ => 'Y', // Will never happen
+            }
+        }
+        'Z' => {
+            // You need to win
+            match opponent_choice {
+                'A' => 'Y',
+                'B' => 'Z',
+                'C' => 'X',
+                _ => 'Z' // will never happen
+            }
+        }
+        _ => 'A', // Will never happen
+    }
+}
+
+pub fn part_one(strategy_guide: &str) -> i32 {
     let mut game_score = 0;
 
     // Step through lines and calculate score
@@ -37,13 +82,36 @@ pub fn play_game(strategy_guide: &str) -> i32 {
     game_score
 }
 
-fn get_tool_name(tool: char) -> &'static str {
-    match tool {
-        'X' | 'A' => "Rock",
-        'Y' | 'B' => "Paper",
-        'Z' | 'C' => "Scissors",
-        _ => "Unknown",
+pub fn part_two(guide: &str) -> i32 {
+    let mut game_score = 0;
+
+    // Step through lines and calculate score
+    for line in guide.lines() {
+        let mut it = line.split_whitespace();
+
+        // Unwrap the first char
+        let opponent_choice = it.next().unwrap().chars().next().unwrap();
+        // Unwrap the second char (Each line is always two chars)
+        let outcome = it.last().unwrap().chars().next().unwrap();
+
+        let choice = get_choice_from_outcome(opponent_choice, outcome);
+
+        let round_score = get_round_score(choice, opponent_choice);
+
+        game_score += round_score;
+
+        println!(
+            "Round: {:?} vs {:?} | Score: {} | Outcome: {:?}",
+            get_tool_name(choice),
+            get_tool_name(opponent_choice),
+            round_score,
+            outcome
+        );
     }
+
+    println!("Total score: {}", game_score);
+
+    game_score
 }
 
 pub fn is_win(choice: char, opponent_choice: char) -> bool {
@@ -88,7 +156,13 @@ mod tests {
 
     #[test]
     fn part_one_works() {
-        let total_score = play_game(TEST_INPUT);
+        let total_score = part_one(TEST_INPUT);
         assert_eq!(total_score, 15);
+    }
+
+    #[test]
+    fn part_two_works() {
+        let total_score = part_two(TEST_INPUT);
+        assert_eq!(total_score, 12);
     }
 }
